558e118f06e3a3fca99af88d74f2b1dab404044a
rahulraj6026 <rahulraj6026@gmail.com>
2020-05-19 21:09:31 +0530

    Last

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PageNotFound() {
     return (
         <div>
-            <h1>404 ERROR: Page Not Found</h1>
+            <h1 style={{color:"red"}}>404 ERROR: Page Not Found</h1>
         </div>
     )
 }
